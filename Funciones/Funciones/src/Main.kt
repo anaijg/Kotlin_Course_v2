e0b342498edd0a726232b17019b8810247b7cec0
anaijg <ana.jimenezgomez@educa.madrid.org>
2024-09-30 17:01:03 +0200

    Summary

--- Funciones/Funciones/src/Main.kt
+++ Funciones/Funciones/src/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
+    // aquí hacemos la llamada
+    val resultado = felicitar( edad = 50 , nombre =  "Asterix")
+    println(resultado)
+}
 
+// ejemplo para declarar una función
+fun felicitar(nombre : String, edad : Int) : String {
+    return "Felicidades,  $nombre, cumples $edad años."
 }
